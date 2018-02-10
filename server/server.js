356efe268d691aa331af6e08fbd37c1cffd47edd
JAdegbohun <feyijimi.adegbohun@gmail.com>
2018-02-10 16:07:18 -0600

    FA: MondoDB DB BackEnd SetUp, the new folder server contains the server.js and routes folder.

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
 
-var index = require(".routes/index");
-var bookings = require(".routes/bookings");
+var index = require("./routes/index");
+var bookings = require("./routes/bookings");
 
 var app = express();
 
