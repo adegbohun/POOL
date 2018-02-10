da6001dabe43f326c76834c0aeeb52492c65c272
JAdegbohun <feyijimi.adegbohun@gmail.com>
2018-02-10 16:53:52 -0600

    FA: MongoDB API Calls

--- server/routes/bookings.js
+++ server/routes/bookings.js
@@ -1,8 +1,16 @@
 var express = require("express");
 var router = express.Router();
+var mongojs = require("mongojs");
+
+var db = mongojs("mongodb://pooladmin:pool01user@ds131698.mlab.com:31698/pool", ["bookings"]);
 
 router.get("/bookings", function(req, res, next){
-    res.send("POOL HOME SCREEN");
+    db.bookings.find(function(err,bookings){
+        if(err){
+            res.send(err);
+        }
+        res.json(bookings);
+    })
 });
 
 module.exports = router;
