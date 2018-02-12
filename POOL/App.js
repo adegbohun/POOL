262c33e606cf2b7a1607822c7a044447be8a5b98
Orajiaku Chukwudalu <chukwudalurjk@gmail.com>
2018-02-11 23:09:18 -0500

    added a base page

--- POOL/App.js
+++ POOL/App.js
@@ -1,20 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View , Image } from 'react-native';
-import Login from './Login';
-import HomeScreen from './HomeScreen';
-import Expo from 'expo';
+import { StyleSheet, Text, View , Image }   from 'react-native';
+import Expo                                 from 'expo';
+import Login                                from './src/screens/Login';
+import HomeScreen                           from './src/screens/HomeScreen';
+import TripsScreen                          from './src/screens/Trips';
+import ProfileScreen                        from './src/screens/Profile';
+import SignUpScreen                         from './src/screens/SignUp';
+import LocationInfoScreen                   from './src/screens/LocationInfo';
+import MessagesScreen                       from './src/screens/Messages';
+import CreateTripScreen                     from './src/screens/CreateTrip';
+import BaseScreen                           from './src/screens/base';
 import {StackNavigator} from 'react-navigation';
 
-const NavigationApp = StackNavigator({
-  Login: { screen: Login},
-  Home: {screen: HomeScreen}
-  
-},
-  { navigationOptions: {
-    header: false,
-  }
+const NavigationApp = StackNavigator(
+    {
+        base: {
+            screen: BaseScreen
+        },
+        profile: {
+            screen: ProfileScreen
+        },
+        sign_up: {
+            screen: SignUpScreen
+        },
+        location_info: {
+            screen: LocationInfoScreen
+        },
+        trips: {
+            screen: TripsScreen
+        },
+        messages: {
+            screen: MessagesScreen
+        },
+        create_trip: {
+            screen: CreateTripScreen
+        },
+        login: {
+            screen: Login
+        },
+        home: {
+            screen: HomeScreen
+        }
+    },
+    {
+      navigationOptions:
+          {
+            header: false,
+          }
 
-});
+    }
+);
 export default class App extends React.Component {
   render() {
     return (
---
+++ POOL/src/screens/base.js
@@ -0,0 +1,116 @@
+/**
+ * Created by orajiakuchukwudalu on 2018-02-11.
+ */
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, ListView, Dimensions} from 'react-native';
+const { height, width } = Dimensions.get('window');
+export default class BaseScreen extends React.Component {
+
+    constructor() {
+        super();
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.state = {
+            dataSource: ds.cloneWithRows(
+                [
+                    {
+                        name: "Home",
+                        route: "home"
+                    },
+                    {
+                        name: "Login",
+                        route: "login"
+                    },
+                    {
+                        name: "Profile",
+                        route: "profile"
+                    },
+                    {
+                        name: "Location info",
+                        route: "location_info"
+                    },
+                    {
+                        name: "Trips",
+                        route: "trips"
+                    },
+                    {
+                        name: "Messages",
+                        route: "messages"
+                    },
+                    {
+                        name: "Create trip",
+                        route: "create_trip"
+                    },
+                    {
+                        name: "Sign Up",
+                        route: "sign_up"
+                    }
+                ]
+            ),
+        };
+    }
+
+    render(){
+
+        return (
+            <View style={styles.container}>
+                <ListView
+                    contentContainerStyle={styles.listView}
+                    dataSource={this.state.dataSource}
+                    renderRow={(rowData) =>
+                        <TouchableOpacity
+                            style={styles.button}
+                            onPress={() => {
+                                this.props.navigation.navigate(rowData.route);
+                            }}
+                        >
+                            <Text
+                                style={styles.buttonText}
+                            >
+                                {rowData.name}
+                            </Text>
+                        </TouchableOpacity>
+                    }
+                />
+            </View>
+        );
+    }
+
+
+}
+
+const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+    },
+    listView: {
+        width,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    button: {
+        width: '80%',
+        height: 50,
+        backgroundColor: 'white',
+        margin: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 5
+    },
+    buttonText: {
+        width: '100%',
+        textAlign: 'center'
+    },
+    container: {
+        height,
+        width,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#2896d3',
+    },
+    text: {
+        color: '#fff',
+    },
+
+
+});
